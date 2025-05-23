de3f1681bccdd07a7f897073d7ef9f1fd60cb3d7
Maria Jose Saad Plata <maria.saad.35@outlook.com>
2025-05-23 17:29:07 -0500

    feat: se añaden atributos adicionales a la clase SistemaAgendamiento

--- src/main/java/org/example/SistemaAgendamiento.java
+++ src/main/java/org/example/SistemaAgendamiento.java
@@ -1,9 +1,12 @@
 package org.example;
 
 public class SistemaAgendamiento {
-    private Usuario[] usuarios;
-    private Medico[] medicos;
-    private Cita[] citas;
+    private Usuario[] usuarios = new Usuario[25];
+    private Medico[] medicos = new Medico[25];
+    private Cita[] citas = new Cita[50];
+    private int cantidadUsuarios = 0;
+    private int cantidadMedicos = 0;
+    private int cantidadCitas = 0;
 
     public SistemaAgendamiento() {
     }
