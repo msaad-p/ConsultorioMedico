7ab3d70e795371c50273deb8286239cffe6954d2
Maria Jose Saad Plata <maria.saad.35@outlook.com>
2025-05-23 18:14:14 -0500

    feat: se añaden los métodos de buscar la posición del usuario, medico y cita dentro de sus respectivos arrays

--- src/main/java/org/example/SistemaAgendamiento.java
+++ src/main/java/org/example/SistemaAgendamiento.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Objects;
+
 public class SistemaAgendamiento {
     private Usuario[] usuarios = new Usuario[25];
     private Medico[] medicos = new Medico[25];
@@ -11,7 +13,32 @@ public class SistemaAgendamiento {
     public SistemaAgendamiento() {
     }
 
+    private int buscarUsuario(String idBuscar) {
+        for (int i = 0; i < cantidadUsuarios; i++) {
+            if (usuarios[i] != null && Objects.equals(usuarios[i].getId(), idBuscar)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
+    private int buscarMedico(String idBuscar) {
+        for (int i = 0; i < cantidadMedicos; i++) {
+            if (medicos[i] != null && Objects.equals(medicos[i].getId(), idBuscar)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int buscarCita(String idBuscar) {
+        for (int i = 0; i < cantidadCitas; i++) {
+            if (citas[i] != null && Objects.equals(citas[i].getId(), idBuscar)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     public Usuario[] getUsuarios() {
         return usuarios;
